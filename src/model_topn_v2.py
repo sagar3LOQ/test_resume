e5d6f04446b0906ae098e952b1e444e16f55f04d
Sagar Sahu <sagar.sahu@3loq.com>
2016-01-06 20:42:26 +0530

    added function get_docvec and some name changes

--- src/model_topn_v2.py
+++ src/model_topn_v2.py
@@ -4,6 +4,7 @@ import gensim
 from gensim.models import Word2Vec
 from sklearn.externals import joblib
 from gensim import utils, matutils
+import random
 import scipy
 from nltk.corpus import stopwords
 import numpy as np
@@ -38,16 +39,16 @@ def cleanse_data(text):
 ##
 
 #	print "cleansing"
-	temp = re.sub(r'[^\x00-\x7F]+',' ', text)
-	temp = re.sub(r'(\d+(\s)?(yrs|year|years|Yrs|Years|Year|yr))'," TIME ",temp)
-	temp = re.sub(r'[\w\.-]+@[\w\.-]+'," EMAIL ",temp)
-	temp = re.sub(r'(((\+91|0)?( |-)?)?\d{10})',' MOBILE ',temp)
-	temp = re.sub(r"[\r\n]+[\s\t]+",'\n',temp)	
+	text = re.sub(r'[^\x00-\x7F]+',' ', text)
+	text = re.sub(r'(\d+(\s)?(yrs|year|years|Yrs|Years|Year|yr))'," TIME ",text)
+	text = re.sub(r'[\w\.-]+@[\w\.-]+'," EMAIL ",text)
+	text = re.sub(r'(((\+91|0)?( |-)?)?\d{10})',' MOBILE ',text)
+	text = re.sub(r"[\r\n]+[\s\t]+",'\n',text)	
 	wF = set(string.punctuation) - set(["+"])
 	for c in wF:
-        	temp =temp.replace(c," ")	
+        	text =text.replace(c," ")	
 
-	return temp
+	return text
 
 
 ## Training data class
@@ -65,11 +66,13 @@ class TrainData():
 
 ## get tfidf model trained from given directory 'dirname' 
 	def get_tfidf_model(self, dirname):
+		dirname = "/home/viswanath/Downloads/total_resume/temp_files"
 		data = Sentences(dirname)
 		tfidf_vectorizer = TfidfVectorizer(stop_words='english')
 		tfidf_matrix = tfidf_vectorizer.fit_transform(data)
 		mat_array = tfidf_matrix.toarray()
 		fn = tfidf_vectorizer.get_feature_names()
+	#	print fn
 		return tfidf_vectorizer
 
 
@@ -80,12 +83,17 @@ class TrainData():
 		label_data = []
 		fn = []
 		for fname in os.listdir(dirname):
-			print "Procession = " + fname
+			print "Processing = " + fname
 			f = open(os.path.join(dirname, fname),"r")
 			raw_text = str.decode(f.read(), "UTF-8", "ignore")
 			text = cleanse_data(raw_text)
 			nword = self.top_n_words_doc(text,tfidf_model,topN)
-			X_coeff = self.gen_LR_weight( nword,text, w2v_model_path,tfidf_model, ndim)
+		#	print nword
+
+	#		X_coeff = self.gen_LR_weight( nword,text, w2v_model_path,tfidf_model, ndim)
+			X_coeff = self.get_docvec( w2v_model_path,tfidf_model, nword, text)
+
+
 			if fname[-10:-4] == 'accept':
 				label = 1
 			else:
@@ -99,19 +107,20 @@ class TrainData():
 # find top N words from TFIDF model
 	def top_n_words_doc(self,text,tfidf_model,topn=20):
 		
-		token = text.split()
+		token = text.lower().split()
 		words = {}
-
+	
 		for w in token:
 			wt = tfidf_model.idf_[tfidf_model.vocabulary_[w]] if w in tfidf_model.vocabulary_ else 0
 			words[w] = wt
 
 		lenw = len(words)
+	#	print lenw, len(token)
 		if (lenw < topn): topn = lenw
 
 		sorted_x = sorted(words.items(), key=operator.itemgetter(1),reverse=True)
 		listd = sorted_x[0:topn]
-
+	#	print listd
 		word= []
 		for i in range((topn)):
 			word.append(listd[i][0])
@@ -119,25 +128,43 @@ class TrainData():
 		return word
 
 
+	def get_docvec(self,w2v_model,tfidf_model, pos_words, text,neg_words=[]):
+		model = self.load_w2vmodel(w2v_model)
+		model_vocab =  tfidf_model.vocabulary_
+
+		X1 = [tfidf_model.idf_[tfidf_model.vocabulary_[i]]* model[i] for i in pos_words if i in model_vocab if i in model.vocab]
+		if len(neg_words) == 0:
+			n_neg = 300
+			neg_words = set(random.sample(model_vocab,n_neg)) - set(pos_words)
+		X2 = [tfidf_model.idf_[tfidf_model.vocabulary_[i]]* model[i] for i in neg_words if i in model.vocab]
+		X = X1 + X2
+		
+		y = [1] * len(X1) + [0] * len(X2)
+	
+
+		regr = LogisticRegression().fit(X, y)
+		docvector = matutils.unitvec(regr.coef_)
+		return docvector
+#		pprint(get_closest_words_to_vec(docvector,model,50))
+
 # Generate wieghts from Logistic Regression Model
 	def gen_LR_weight(self, words,text, w2v_model,tfidf_model, ndim):
 		model = self.load_w2vmodel(w2v_model)
-		temp = text.lower().split()
+		text = text.lower().split()
 
 		notfound = 0
 		denom = 0
 		nvecs = 0
 		label = []
-		avg_vec = []
+		doc_vec = []
 		wt =0
-		for w in temp:
-
+	#	print len(text), len(words)
+		for w in text:
 			if w in model:
-
 				wt = tfidf_model.idf_[tfidf_model.vocabulary_[w]] if w in tfidf_model.vocabulary_ else 0
 				ve = np.multiply(model[w], wt)
 
-				avg_vec.append(ve)				
+				doc_vec.append(ve)				
 				if w in words: 
 					label.append(1)
 				else: 
@@ -148,8 +175,8 @@ class TrainData():
 				notfound += 1
 		if denom == 0:
 			return []
-
-		logit = LogisticRegression(C=1.0).fit(avg_vec, label)
+		
+		logit = LogisticRegression(C=1.0).fit(doc_vec, label)
 
 		return logit.coef_
 
@@ -225,7 +252,7 @@ class PredictData():
 # Generate wieghts from Logistic Regression Model
 	def gen_LR_weight(self, words,text, w2v_model,tfidf_model, ndim):
 		model = self.load_w2vmodel(w2v_model)
-		temp = text.lower().split()
+		text = text.lower().split()
 
 		notfound = 0
 		denom = 0
@@ -233,7 +260,7 @@ class PredictData():
 		label = []
 		avg_vec = []
 		wt =0
-		for w in temp:
+		for w in text:
 
 			if w in model:
 
@@ -334,7 +361,7 @@ class genTopNVec:
 
 	def getAnalysis(self,true_pred,y_pred1):
 		precision, recall, fscore, support = score(true_pred,y_pred1)
-		return [matthews_corrcoef( true_pred,y_pred1),roc_auc_score( true_pred,y_pred1),precision[0],precision[1],recall[0],recall[1],fscore[0],fscore[1],support[0],support[1]]
+		return matthews_corrcoef( true_pred,y_pred1),roc_auc_score( true_pred,y_pred1),precision[0],precision[1],recall[0],recall[1],fscore[0],fscore[1],support[0],support[1]
 
 	def printResult(self):
 		
@@ -371,20 +398,25 @@ class genTopNVec:
 		input_filename.close()
 
 
-	def NFoldTest(self, iter_N=5,split =0.3,random_state=0):
+	def NFoldTest(self, iter_N=5,split =0.30,random_state=0):
 
-		total_dirname = '/home/viswanath/workspace/resume_data/res_dir/total'
+		total_dirname = '/home/viswanath/workspace/test_resume/train'
 		td = TrainData()
+		print "extracting data"
 		x_total, y_total, fn_total  = td.train_model( total_dirname, self.w2v_model_path,self.topN,self.size)
-
+		print "random split"
 		kf_total = cross_validation.ShuffleSplit(len(x_total), n_iter=iter_N, test_size=split,   random_state=random_state)
 		x_tot_np = Math.array(x_total)
 		y_tot_np = Math.array(y_total)
 		
 		j =0
 		fn_test = []
+		fnRes = "Output_topN_" + str(self.topN)+"_split_"+str(split*100) + ".tsv"
+		fp = open(fnRes,"wb")
+		fp.write("matthews_corrcoef\troc_auc_score\tprecision[0]\tprecision[1]\trecall[0]\trecall[1]\tfscore[0]\tfscore[1]\tsupport_0\tsupport_1\n")
 		for train, test in kf_total:
 			j += 1
+			print "Case:: " + str(j) +"\n\n"
 			lgr = LogisticRegression(C=1.0).fit(x_tot_np[train],y_tot_np[train])
 			y_pred = lgr.predict(x_tot_np[test])
 			y_test = y_tot_np[test]
@@ -394,12 +426,13 @@ class genTopNVec:
 				fn_test.append(fn_total[i])
 				k += 1
 
-			fn = "Output_topN_" + str(j) + ".tsv"
-
-			self.saveNFoldResult2file(y_test,y_pred,fn_test,fn)
-
-
-
+		#	fn = "Output_topN_" + str(j) + ".tsv"
+			print "writing data to file"
+			#self.saveNFoldResult2file(y_test,y_pred,fn_test,fn)
+			#self.printAnalysis(y_test,y_pred)
+			matthews_corrcoef, roc_auc_score, precision_0 , precision_1 , recall_0 , recall_1 , fscore_0 , fscore_1 , support_0, support_1 = self.getAnalysis(y_test,y_pred)
+			Resultstr = str(matthews_corrcoef)+"\t"+str(roc_auc_score)+"\t"+str(precision_0)+"\t"+str(precision_1)+"\t"+str(recall_0)+"\t"+str(recall_1)+"\t"+str(fscore_0)+"\t"+str(fscore_1)+"\t"+str(support_0)+"\t"+str(support_1)+"\n"
+			fp.write(Resultstr)
 
 if __name__ == '__main__': 
 
@@ -409,12 +442,14 @@ if __name__ == '__main__':
 #	total_dirname = '/home/viswanath/workspace/resume_data/res_dir/total'
 	test_dirname = ''
 	predict_dirname = '/home/viswanath/workspace/test_resume/predict'
-	w2v_model_path = '/home/viswanath/workspace/test_resume/model/w2v_model_50.mod'
-	size = 52
-	topNA = [200,300,500,1000]  
+	w2v_model_path = '/home/viswanath/workspace/test_resume/model/w2v_model_100.mod'
+	size = 100
+	topNA = [300]  
 	for topN in topNA:
+		print "\nFor TopN N=" + str(topN) + "\n"
 		gt = genTopNVec(train_dirname,test_dirname,predict_dirname,w2v_model_path,size,topN)
-		gt.train_predict()
+		gt.NFoldTest(iter_N=10,split =0.40)
+
 #	gt.printResult()
 #	gt.saveResult2file("topN_result.tsv")
 #	gt.NFoldTest()
